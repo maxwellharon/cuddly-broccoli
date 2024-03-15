5b0953de1207093582cdf1c967a8b1a428617281
maxwellharon <maxwellharon54@gmail.com>
2024-03-15 14:06:46 +0300

    init

---
+++ frontend/src/components/Footer.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+/* REACT-BOOTSTRAP */
+import { Container, Row, Col } from "react-bootstrap";
+
+function Footer() {
+  return (
+    <footer>
+      <Container>
+        <Row>
+        </Row>
+      </Container>
+    </footer>
+  );
+}
+
+export default Footer;
